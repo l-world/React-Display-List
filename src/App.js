965dae7c039e51c202e84c9a530a4314ce3935ac
echo <heart0magic@163.com>
2022-06-15 21:44:13 +0800

    first commit display-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
+  const names = ['Naveh', 'Echo','Leo','Amit Roushan','Lsabel','Eric','Mihi','Chris Denney'];
+  const lis = names.map( name => {
+      return <li>{name}</li>
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='wrap'>
+        <h1>React-Display-List</h1>
+        <ul className='name--list'>
+        {lis}
+        </ul>
+    </section>
+  )
 }
-
-export default App;
